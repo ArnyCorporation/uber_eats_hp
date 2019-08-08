2d685721dcd8e118e0d77b53f3c71591c308d21f
Max_Arn <maximarnayt@gmail.com>
2019-08-08 21:33:52 +0300

    add smooth scroll and add more smooth for hover elements

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -13,10 +13,9 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+const btn = document.getElementById('myBtn');
 
-topFunction();
+btn.addEventListener('click', () => window.scrollTo({
+  top: 0,
+  behavior: 'smooth',
+}));
